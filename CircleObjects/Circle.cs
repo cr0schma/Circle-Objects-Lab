20cf3426f91c26dd42f1e04207fb18f42a95f98c
Chris Schmalz <cschmalz03@gmail.com>
2023-02-09 19:32:53 -0500

    initial commit

---
+++ CircleObjects/Circle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace CircleObjects
+{
+    internal class Circle
+    {
+        private double radius;
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        // Anthony said just these methods were okay to use
+        public double CalculateCircumference()
+        {
+            double circumference = Math.Round((2 * Math.PI * GetRadius()), 2);
+            return circumference;
+        }
+
+        public double CalculateArea()
+        {
+            double area = Math.Round((Math.PI * Math.Pow(GetRadius(), 2)), 2);
+            return area;
+        }
+
+        public double GetRadius() { return radius; }
+    }
+}
---
+++ CircleObjects/Program.cs
@@ -0,0 +1,38 @@
+﻿using CircleObjects;
+
+Console.WriteLine("Welcome to the Circle Tester");
+int counter = 1;
+for (int i = 0; i < counter; i++)
+{
+    bool playagain = true;
+    while (playagain)
+    {
+        Console.Write("Enter radius: ");
+        try
+        {
+            double userRadius = double.Parse(Console.ReadLine());
+            Circle userCircle = new(userRadius);
+
+            Console.WriteLine($" Circumference: {userCircle.CalculateCircumference()}");
+            Console.WriteLine($" Area: {userCircle.CalculateArea()}");
+            Console.Write("Continue? (y/n): ");
+            string answer = Console.ReadLine();
+            if (answer == "y")
+            {
+                counter = counter + 1;
+            }
+            else
+            {
+                playagain = false;
+                Console.WriteLine($"Goodbye. You created {counter} Circle object(s).");
+                counter = 0;
+            }
+        }
+        catch
+        {
+            Console.Write("Invalid data, try again? (y/n): ");
+            string answer = Console.ReadLine();
+            if (answer == "y") { } else { playagain = false; }
+        }
+    }
+}
